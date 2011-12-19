4cedb8a36715d662041ad0e2dadc618735c8f13f
kongyuan <anubiskong@gmail.com>
2011-12-19 20:05:29 +0800

    修正清理缓存不见查目录的问题

--- rmcache.sh
+++ rmcache.sh
@@ -11,6 +11,8 @@ fi
 
 OLDPATH=$(pwd)
 
+if [ -d "${HEREPATH/data}" ] && [ -d "${HEREPATH}/data/cache" ] && [ -d "${HEREPATH}/data/class" ] && [ -d "${HEREPATH}/data/compiled" ]
+	then
 cd "${HEREPATH}/data/cache";
 rm -r *;
 cd $OLDPATH;
@@ -20,5 +22,7 @@ cd $OLDPATH;
 cd "${HEREPATH}/data/compiled";
 rm -r *;
 cd $OLDPATH;
-
-
+echo "成功完成清理";
+else
+echo "缓存目录不存在,请检查拼写和权限";
+fi
