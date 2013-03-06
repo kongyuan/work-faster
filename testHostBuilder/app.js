9a6abbd11069fb16df13c2f2bcda5d01bb4b9587
Anubis <anubiskong@gmail.com>
2013-03-07 00:02:51 +0800

    利用express的static中间件简化实现

--- testHostBuilder/app.js
+++ testHostBuilder/app.js
@@ -1,89 +1,16 @@
 var PORT = 8000;
 var http = require("http");
-var url = require("url");
-var fs = require("fs");
-var path = require("path");
-var mime = require("./mime").types;
-var config = require("./config");
-var zlib = require("zlib");
+var fs = require('fs');
+var express = require('express');
 var argv = process.argv.splice(2);
 var serverPath = "";
 var spawn = require('child_process').spawn;
 
-var server = http.createServer(function(request, response) {
-    response.setHeader("Server", "SimpleHTTPServer");
-    var pathname = url.parse(request.url).pathname;
-    // console.log(pathname);
-    if (pathname.slice(-1) === "/") {
-        pathname = pathname + config.Welcome.file;
-    }
-    if(!serverPath){
-        console.log("error!  have no server path!!");
-        return;
-    }
-    var realPath = path.join(serverPath, path.normalize(pathname.replace(/\.\./g, "")));
-
-    var pathHandle = function (realPath) {
-        fs.stat(realPath, function (err, stats) {
-            if (err) {
-                response.writeHead(404, "Not Found", {'Content-Type': 'text/plain'});
-                response.write("This request URL " + pathname + " was not found on this server.");
-                response.end();
-            } else {
-                if (stats.isDirectory()) {
-                    realPath = path.join(realPath, "/", config.Welcome.file);
-                    pathHandle(realPath);
-                } else {
-                    var ext = path.extname(realPath);
-                    ext = ext ? ext.slice(1) : 'unknown';
-                    var contentType = mime[ext] || "text/plain";
-                    response.setHeader("Content-Type", contentType);
-
-                    var lastModified = stats.mtime.toUTCString();
-                    var ifModifiedSince = "If-Modified-Since".toLowerCase();
-                    response.setHeader("Last-Modified", lastModified);
-
-                    if (ext.match(config.Expires.fileMatch)) {
-                        var expires = new Date();
-                        expires.setTime(expires.getTime() + config.Expires.maxAge * 1000);
-                        response.setHeader("Expires", expires.toUTCString());
-                        response.setHeader("Cache-Control", "max-age=" + config.Expires.maxAge);
-                    }
-
-                    if (request.headers[ifModifiedSince] && lastModified == request.headers[ifModifiedSince]) {
-                        response.writeHead(304, "Not Modified");
-                        response.end();
-                    } else {
-                        var raw = fs.createReadStream(realPath);
-                        var acceptEncoding = request.headers['accept-encoding'] || "";
-                        var matched = ext.match(config.Compress.match);
-
-                        if (matched && acceptEncoding.match(/\bgzip\b/)) {
-                            response.writeHead(200, "Ok", {'Content-Encoding': 'gzip'});
-                            raw.pipe(zlib.createGzip()).pipe(response);
-                        } else if (matched && acceptEncoding.match(/\bdeflate\b/)) {
-                            response.writeHead(200, "Ok", {'Content-Encoding': 'deflate'});
-                            raw.pipe(zlib.createDeflate()).pipe(response);
-                        } else {
-                            response.writeHead(200, "Ok");
-                            raw.pipe(response);
-                        }
-                    }
-                }
-            }
-        });
-    };
-
-    pathHandle(realPath);
-});
-
 function createBaseFiles(){
-    //todo
     console.log("start create files");
 
     spawn("mkdir" , [serverPath + "/css"]);
     spawn("mkdir" , [serverPath + "/js"]);
-
 
     options = {
         host: 'code.jquery.com' ,
@@ -101,9 +28,11 @@ function createBaseFiles(){
 
         res.on('end', function(){
             fs.writeFile( serverPath + "/js/jquery.js", imagedata, 'binary', function(err){
-                if (err) throw err;
+                if (err)
+                    console.log ('download error: ' , err);
+                    spawn("cp" , [ 'file/jquery.js' , serverPath + "/js"]);
                 console.log("download jquery last version success.");
-                startServer();
+                startServer(serverPath);
             });
         });
     });
@@ -128,9 +57,17 @@ function createBaseFiles(){
     });
 }
 
-function startServer(){
-    server.listen(PORT);
-    console.log("Server runing at port: " + PORT + ".");
+function startServer(dir){
+    var app = express();
+    app.configure( function(){
+        app.set('port', PORT);
+        app.use(express.cookieParser());
+        app.use(express.directory(dir));
+        app.use(express.static(dir));
+    });
+    http.createServer(app).listen( PORT , function(){
+        console.log( 'start server on :' + dir);
+    });
 }
 
 function rollback(){
@@ -145,15 +82,12 @@ if(!argv[0]){
     console.log ("error! give me a path !!");
     return;
 }else{
-
     serverPath = argv[0];
     fs.mkdir( serverPath , 0777 , function(err){
-
         if(err){
             if(err['code'] === 'EEXIST'){
                 console.log('dir exist , pass create dir');
-                //TODO exist
-                startServer();
+                startServer(serverPath);
             }else{
                 throw err;
             }
